0fa9173d8c562dcda7eaf686919cc96169ff3739
Phoebe <jolinhappy1990@hotmail.com>
2024-06-10 22:44:38 +0800

    feat: add textarea component and story

---
+++ src/stories/web-components/Textarea.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Textarea from 'SRC/components/ui/Textarea';
+
+const meta: Meta<typeof Textarea> = {
+  title: 'web-components/Textarea',
+  component: Textarea,
+  argTypes: {
+    placeholder: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    maxLength: {
+      control: 'number',
+    },
+    value: {
+      control: 'text',
+    },
+  },
+  args: {
+    placeholder: 'placeholder',
+    disabled: false,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
