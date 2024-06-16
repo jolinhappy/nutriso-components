c5971f4dc2567ed57fdbd0f1909ef27f5a05145c
Phoebe <jolinhappy1990@hotmail.com>
2024-06-16 10:52:58 +0800

    feat: add toast component and story

---
+++ src/components/ui/Toast/index.ts
@@ -0,0 +1,5 @@
+import Toaster from './Toaster';
+import { ToastAction } from './Toast';
+import { useToast } from './useToast';
+
+export { Toaster, ToastAction, useToast };
---
+++ src/stories/web-components/Toast.stories.tsx
@@ -0,0 +1,93 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Toaster, useToast } from 'SRC/components/ui/Toast';
+import Button from 'SRC/components/ui/Button';
+
+const meta: Meta<typeof Toaster> = {
+  title: 'web-components/Toast',
+  component: Toaster,
+  argTypes: {
+    title: {
+      control: 'text',
+    },
+    description: {
+      control: 'text',
+    },
+    action: {
+      control: 'object',
+    },
+    variant: {
+      control: 'radio',
+      options: ['primary', 'warning', 'error'],
+    },
+    isShowClose: {
+      control: 'boolean',
+    },
+  },
+  args: {
+    isShowClose: false,
+    title: 'title',
+    description: 'description',
+    variant: 'info',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'title',
+    description: 'description',
+  },
+  render: (args) => {
+    const { toast } = useToast();
+    const { title, description, ...otherArgs } = args;
+    return (
+      <>
+        <Button
+          className="mr-2"
+          onClick={() => {
+            toast({
+              title,
+              description,
+              ...otherArgs,
+            });
+          }}
+        >
+          open modal
+        </Button>
+        <Toaster />
+      </>
+    );
+  },
+};
+
+export const WithCloseButton: Story = {
+  args: {
+    title: 'title',
+    description: 'description',
+    isShowClose: true,
+  },
+  render: (args) => {
+    const { toast } = useToast();
+    const { title, description, variant, ...otherArgs } = args;
+    return (
+      <>
+        <Button
+          className="mr-2"
+          onClick={() => {
+            toast({
+              title,
+              description,
+              variant,
+              ...otherArgs,
+            });
+          }}
+        >
+          open modal
+        </Button>
+        <Toaster />
+      </>
+    );
+  },
+};
