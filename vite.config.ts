cf315c0bc093ff4ab3804f82b76ec92acacdd189
Phoebe <jolinhappy1990@hotmail.com>
2024-06-29 15:17:11 +0800

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
@@ -16,6 +17,11 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    environmentMatchGlobs: [['**/*.test.tsx', 'jsdom']],
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
